6bb6d55f9e133c98fb5dd391e0679d5d1fe17352
Maria Barra <mari.sws1999@gmail.com>
2026-07-30 00:34:02 -0400

    Delete Python directory

--- Python/edad.py
+++
@@ -1,8 +0,0 @@
-edad = int(input("Ingrese una edad: "))
-
-if edad >= 18:
-    print("La persona es mayor de edad")
-
-else:
-    print("La persona es menor de edad")
-    
--- Python/horasyminutosensegundos.py
+++
@@ -1,8 +0,0 @@
-horas = int(input("Ingrese las horas: "))
-minutos = int(input("Ingrese los minutos: "))
-segundos = int(input("Ingrese los segundos: "))
-
-#Calculamos el total convirtiendo todo a segundos
-resultado = (horas * 3600)+ (minutos * 60) + segundos
-
-print("El total en segundos es: ", resultado)
